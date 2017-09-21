eb572bab40c73b70641a9b22160daedf887a0290
casey369 <casey369@morris.umn.edu>
2017-09-21 11:38:41 -0500

    Fixes memory leak in main.c and begins countVowel method in disemvowel.c

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -1,7 +1,22 @@
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "disemvowel.h"
 
+
+int countVowels(char* str){
+	int counter = 0;
+	for(unsigned i = 0; i < sizeof(str);i++){
+		if(str[i] == 'a' || str[i] == 'e' || str[i] == 'i' || str[i] == 'o' || str[i] == 'u'){
+			counter++;
+		}
+	}
+	printf(counter + "");
+	return counter;
+}
+
+			
 char* disemvowel(char* str) {
+//printf(countVowels(str) +"");
   return (char*) "";
 }
