5eb775da266a515c6b1afb4b8e88ecd2e7dbb4ea
casey369 <casey369@morris.umn.edu>
2017-09-28 10:52:07 -0500

    removes unnecessary code from diemvowel.c

--- disemvowel/disemvowel.c
+++ disemvowel/disemvowel.c
@@ -6,8 +6,8 @@
 
 char* trashVowels(char* str){
     int len = strlen(str);
-     char* withoutVowels = (char*)calloc(len + 1,sizeof(str));
-      withoutVowels[0] = '\0';
+    char* withoutVowels = (char*)calloc(len + 1,sizeof(str));
+    withoutVowels[0] = '\0';
     int i, j=0;
 	for(i = 0; i < len;i++){
 		if(str[i] != 'a' && str[i] != 'e' && str[i] != 'i' && str[i] != 'o' && str[i] != 'u' &&
@@ -16,12 +16,10 @@ char* trashVowels(char* str){
                     j++;
 		}
 	}
-	printf("%d",j);
 	withoutVowels[j] = '\0';
         return withoutVowels;
 }
 			
 char* disemvowel(char* str) {
 return trashVowels(str);
-//return (char*) "";
 }
